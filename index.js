249e6db66cd78a1b47923ae9e82877779e4141c4
gxlself <gxlself@163.com>
2021-09-26 10:42:16 +0800

    feat: 新增对指定token操作

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import Cookies from 'js-cookie';
 import { COMMON_DOMIN, TOKEN_NAME } from '@cyberspace.link/env';
 export function getToken () { return Cookies.get(TOKEN_NAME) }
 export function setToken (token) { Cookies.set(TOKEN_NAME, token, { domain: COMMON_DOMIN }) }
-export function removeToken() { Cookies.remove(TOKEN_NAME); return Cookies.remove(TOKEN_NAME, { domain: COMMON_DOMIN })}
+export function removeToken() { Cookies.remove(TOKEN_NAME); return Cookies.remove(TOKEN_NAME, { domain: COMMON_DOMIN }) }
+export function getCustomToken(name) { return Cookies.get(name)}
+export function setCustomToken(name, token) { Cookies.set(name, token) }
+export function removeCustomToken(name) { Cookies.remove(name) }
